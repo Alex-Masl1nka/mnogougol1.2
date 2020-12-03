196d8deee4c101df2c9df95b2ca9a3a93ba079d2
Alex-Masl1nka <65232033+Alex-Masl1nka@users.noreply.github.com>
2020-12-03 13:58:28 +0300

    da

---
+++ Class1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace многоугольники
+{
+    public abstract class Draw
+    {
+        protected int x, y;
+        protected static int r;
+        public Draw(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        static Draw()
+        { r = 25; }
+        public int SetX { get { return x; } set { x = value; } }
+        public int SetY { get { return y; } set { y = value; } }
+        public abstract void DrawFigure(Graphics graf);
+        abstract public bool check(int x, int y);
+    }
+    public class circle : Draw
+    {
+        SolidBrush br = new SolidBrush(Color.Green);
+        public circle(int x, int y) : base(x, y)
+        {
+        }
+        public override void DrawFigure(Graphics graf)
+        {
+            graf.FillEllipse ( new SolidBrush(Color.Green), x - (r / 2), y - (r / 2), r, r);
+        }
+        public override bool check(int x, int y)
+        {
+            if (Math.Sqrt(Math.Pow(x - this.x - (r / 2), 2) + Math.Pow(y - this.y - (r / 2), 2)) < r) return true;
+            else return false;
+        }
+    }
+    public class square : Draw
+    {
+        SolidBrush br = new SolidBrush(Color.Green);
+        public square(int x, int y) : base(x, y)
+        {
+        }
+        public override void DrawFigure(Graphics graf)
+        {
+            graf.FillRectangle(new SolidBrush(Color.Green), x - (r / 2), y - (r / 2), r, r);
+        }
+        public override bool check(int x, int y)
+        {
+            if (this.x - (r / 2) < x && this.x + (r / 2) > x && this.y - (r / 2) < y && this.y + (r / 2) > y) return true;
+            else return false;
+        }
+    }
+    public class triangle : Draw
+    {
+        SolidBrush br = new SolidBrush(Color.Green);
+        public triangle(int x, int y) : base(x, y)
+        {
+        }
+        public override void DrawFigure(Graphics graf)
+        {
+            Point point1 = new Point(x - r, y + (r / 2));
+            Point point2 = new Point(x + r, y + (r / 2));
+            Point point3 = new Point(x, y - r);
+            Point[] Points = { point1, point2, point3 };
+            graf.FillPolygon(new SolidBrush(Color.Green), Points);
+        }
+        public override bool check(int x, int y)
+        {
+            if (this.x - (r / 2) < x && this.x + (r / 2) > x && this.y - r < y && this.y + (r / 2) > y) return true;
+            else return false;
+        }
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace многоугольники
+{
+    public partial class Form1 : Form
+
+    {
+        //Draw shape;
+        int i, del_X, del_Y, amount;
+        bool Dodraw, grab, NotNull=false;
+        List<Draw> versh;
+        public Form1()
+        {
+            InitializeComponent();
+            del_X = 0;
+            del_Y = 0;
+            Dodraw = false;
+            grab = false;
+            versh = new List<Draw>();
+            i = 0;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DoubleBuffered = true;
+        } 
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (grab)
+            {
+                versh[amount].SetX = e.X - del_X;
+                versh[amount].SetY = e.Y - del_Y;
+                Refresh();
+            }
+        }
+
+        private void окружностьToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            i = 0;
+        }
+
+        private void квадратToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            i = 1;
+        }
+
+        private void треугольникToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            i = 2;
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            for (int j = 0; j < versh.Count; j++)
+                {
+                amount = j;
+                NotNull = true;
+                }
+            if (Dodraw && versh[amount].check(e.X, e.Y)&& NotNull == true)
+            {
+                if (e.Button == MouseButtons.Left)
+                {
+                    del_X = e.X - versh[amount].SetX;
+                    del_Y = e.Y - versh[amount].SetY;
+                    grab = true;
+                }
+                if (e.Button == MouseButtons.Right)
+                {
+                    versh.RemoveAt(amount);
+                    if (versh.Count == 0)
+                    {
+                        Dodraw = false;
+                        NotNull = false;
+                    }
+                }
+            }
+            else
+            {
+                switch (i)
+                {
+                    case 0: versh.Add(new circle(e.X, e.Y)); break;
+                    case 1: versh.Add(new square(e.X, e.Y)); break;
+                    case 2: versh.Add(new triangle(e.X, e.Y)); break;
+                    default: versh.Add(new circle(e.X, e.Y)); break;
+                }
+                Dodraw = true;
+            }
+            Refresh();
+        }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            if(Dodraw)
+            {
+                for (int j = 0; j < versh.Count; j++)
+                {
+                    Graphics g = e.Graphics;
+                    versh[j].DrawFigure(g);
+                }
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if(grab)
+            {
+                grab = false;
+                Refresh();
+            }
+        }
+    }
+}
